810fc83a96f13c12fb3c61be4ad888a4bfce810e
Aisak <santaellaisaac2@gmail.com>
2022-10-01 12:17:06 -0400

    Ready Tour of Heroes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component:DashboardComponent},
+  { path:'heroes', component:HeroesComponent}
+];
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes) 
+  ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule { }
